3253f6c7ca94b9ad06f8e7443f041c73e9eea819
Jason Kim <jasonkim0174@gmail.com>
2024-10-20 23:50:14 -0400

    fixed static

--- cs412/urls.py
+++ cs412/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path("quotes/", include("quotes.urls")),
     path("restaurant/", include("restaurant.urls")),
     path("mini_fb/", include("mini_fb.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL,
+           document_root=settings.STATIC_ROOT)
 
 
